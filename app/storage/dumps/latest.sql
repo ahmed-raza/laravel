873a91364f9988e79d0bbe5a88a8a8de59a01448
Ahmed Raza <ahmed.raza@square63.com>
2015-01-08 17:14:00 +0500

    Latest DB

--- app/storage/dumps/latest.sql
+++ app/storage/dumps/latest.sql
@@ -43,6 +43,33 @@ INSERT INTO `authors` VALUES (1,'Ahmed Raza','Vivamus congue porta felis eu dign
 UNLOCK TABLES;
 
 --
+-- Table structure for table `gallery`
+--
+
+DROP TABLE IF EXISTS `gallery`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gallery` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `img_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gallery`
+--
+
+LOCK TABLES `gallery` WRITE;
+/*!40000 ALTER TABLE `gallery` DISABLE KEYS */;
+INSERT INTO `gallery` VALUES (7,1,'1_342854,xcitefun-lamborghini-huracan-lp610-4-3.jpg','2015-01-08 02:29:13','2015-01-08 02:29:13'),(8,1,'1_342855,xcitefun-lamborghini-huracan-lp610-4-2.jpg','2015-01-08 02:46:29','2015-01-08 02:46:29'),(9,1,'1_342856,xcitefun-lamborghini-huracan-lp610-4-1.jpg','2015-01-08 02:53:37','2015-01-08 02:53:37'),(10,2,'2_further_afield_slider_logo.png','2015-01-08 03:18:50','2015-01-08 03:18:50');
+/*!40000 ALTER TABLE `gallery` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -61,7 +88,7 @@ CREATE TABLE `migrations` (
 
 LOCK TABLES `migrations` WRITE;
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-INSERT INTO `migrations` VALUES ('2014_09_19_072623_create_users_table',1),('2014_09_23_044730_create_authors_table',1),('2015_01_07_052936_create_posts_table',2);
+INSERT INTO `migrations` VALUES ('2014_09_19_072623_create_users_table',1),('2014_09_23_044730_create_authors_table',1),('2015_01_07_052936_create_posts_table',2),('2015_01_08_063507_create_gallery_table',3);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,7 +110,7 @@ CREATE TABLE `posts` (
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -92,7 +119,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES (6,'Main Slider','<p>This is just an example post of this laravel blog.</p>','','first post','main-slider',1,'2015-01-07 04:31:36','2015-01-07 04:31:36');
+INSERT INTO `posts` VALUES (6,'Main Slider','<p>This is just an example post of this laravel blog.</p>','','first post','main-slider',1,'2015-01-07 04:31:36','2015-01-07 04:31:36'),(8,'Example Post','<p>Heheheheeheheh hahahahaha</p><ol><li><strong><em>Yolo</em></strong></li><li><strong><em>Swag</em></strong></li></ol><ul><li><strong><em>Hahaha</em></strong></li></ul>','','','example-post',1,'2015-01-07 05:56:11','2015-01-07 05:56:11'),(9,'Maecenas mollis ipsum amet.','<p>Nunc interdum lacus sit amet orci. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Curabitur vestibulum aliquam leo. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Fusce neque.</p><p>Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Nunc nulla. Phasellus tempus. Ut id nisl quis enim dignissim sagittis. Curabitur at lacus ac velit ornare lobortis.</p><p>Quisque rutrum. Vivamus elementum semper nisi. Suspendisse feugiat. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Fusce fermentum.</p>','','Praesent iaculis purus sed.','maecenas-mollis-ipsum-amet',1,'2015-01-07 07:34:35','2015-01-07 07:34:35'),(10,'Duis vestibulum auctor sed.','<p>Nunc interdum lacus sit amet orci. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Curabitur vestibulum aliquam leo. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Fusce neque.</p><p>Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Nunc nulla. Phasellus tempus. Ut id nisl quis enim dignissim sagittis. Curabitur at lacus ac velit ornare lobortis.</p><p>Quisque rutrum. Vivamus elementum semper nisi. Suspendisse feugiat. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Fusce fermentum.</p>','','Vestibulum orci ex; nullam.','duis-vestibulum-auctor-sed',1,'2015-01-07 07:34:55','2015-01-07 07:34:55'),(11,'In hac habitasse cras amet.','<p>Nunc interdum lacus sit amet orci. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Curabitur vestibulum aliquam leo. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Fusce neque.</p><p>Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Nunc nulla. Phasellus tempus. Ut id nisl quis enim dignissim sagittis. Curabitur at lacus ac velit ornare lobortis.</p><p>Quisque rutrum. Vivamus elementum semper nisi. Suspendisse feugiat. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Fusce fermentum.</p>','','','in-hac-habitasse-cras-amet',1,'2015-01-07 07:35:02','2015-01-07 07:35:02'),(12,'Vestibulum ante ipsum amet.','<p>Nunc interdum lacus sit amet orci. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Curabitur vestibulum aliquam leo. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Fusce neque.</p><p>Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Nunc nulla. Phasellus tempus. Ut id nisl quis enim dignissim sagittis. Curabitur at lacus ac velit ornare lobortis.</p><p>Quisque rutrum. Vivamus elementum semper nisi. Suspendisse feugiat. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Fusce fermentum.</p>','','','vestibulum-ante-ipsum-amet',1,'2015-01-07 07:35:09','2015-01-07 07:35:09'),(13,'Quisque ut arcu in ex amet.','<p>Nunc interdum lacus sit amet orci. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Curabitur vestibulum aliquam leo. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Fusce neque.</p><p>Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Nunc nulla. Phasellus tempus. Ut id nisl quis enim dignissim sagittis. Curabitur at lacus ac velit ornare lobortis.</p><p>Quisque rutrum. Vivamus elementum semper nisi. Suspendisse feugiat. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Fusce fermentum.</p>','','','quisque-ut-arcu-in-ex-amet',1,'2015-01-07 07:35:16','2015-01-07 07:35:16');
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -126,7 +153,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Admin','sadmin','ahmed.raza@square63.com','03244256607','Lahore','-1','Maecenas vestibulum eros sit amet eros pellentesque euismod? Integer sit amet ante sit amet nulla euismod bibendum. Nunc condimentum dignissim nunc vel consequat. Sed vitae condimentum odio; et hendrerit neque. Suspendisse et tempor risus. Cras eu sem in lacus luctus cursus consequat sit amet massa. Nam hendrerit venenatis nunc sed suscipit. Morbi auctor tempor diam ultricies viverra. Donec sagittis lectus eu malesuada mattis. Quisque libero elit, sagittis venenatis justo at, dignissim volutpat.\r\n','$2y$10$.wlZ7LZ4JAPHbsPLNevrvebhOG8Kit8FmYMKxxMYMGTU56C.SS50q','0000-00-00 00:00:00','2015-01-06 03:35:13','UtbRUVpLPuI5YxYjDvRV3xEBvggOVjBvlQCnFm8yoIZgdzi8VNrLPIgnkm6H'),(2,'Raza','user','raza1778@gmail.com','+92323456789','Lahore','-1','Vivamus euismod mauris. Vestibulum rutrum, mi nec elementum vehicula, eros quam gravida nisl, id fringilla neque ante vel mi. Vivamus quis mi. Mauris sollicitudin fermentum libero. Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel, lacus.','$2y$10$DXNbJR8sK.aNEGY9EJAcy.JaMcuKDTZsS6V7U7W7oZf4Cm.CpH.BW','0000-00-00 00:00:00','2015-01-05 07:14:54','naKFb7lKsnMXH520TUTFKACmY3gW9qo5yyW8SjmOieloQAZxXvrzJXDUpDQU');
+INSERT INTO `users` VALUES (1,'Admin','sadmin','ahmed.raza@square63.com','03244256607','Lahore','-1','Maecenas vestibulum eros sit amet eros pellentesque euismod? Integer sit amet ante sit amet nulla euismod bibendum. Nunc condimentum dignissim nunc vel consequat. Sed vitae condimentum odio; et hendrerit neque. Suspendisse et tempor risus. Cras eu sem in lacus luctus cursus consequat sit amet massa. Nam hendrerit venenatis nunc sed suscipit. Morbi auctor tempor diam ultricies viverra. Donec sagittis lectus eu malesuada mattis. Quisque libero elit, sagittis venenatis justo at, dignissim volutpat.\r\n','$2y$10$.wlZ7LZ4JAPHbsPLNevrvebhOG8Kit8FmYMKxxMYMGTU56C.SS50q','0000-00-00 00:00:00','2015-01-08 05:05:39','Vl85vXWu1LJFMvbTIWeaSfW06hnMIKPNOhl2gDNeX1hD2eqdEiz5L8L2HGUt'),(2,'Raza','user','raza1778@gmail.com','+92323456789','Lahore','-1','Vivamus euismod mauris. Vestibulum rutrum, mi nec elementum vehicula, eros quam gravida nisl, id fringilla neque ante vel mi. Vivamus quis mi. Mauris sollicitudin fermentum libero. Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel, lacus.','$2y$10$DXNbJR8sK.aNEGY9EJAcy.JaMcuKDTZsS6V7U7W7oZf4Cm.CpH.BW','0000-00-00 00:00:00','2015-01-08 05:00:18','wmaY8xe5GtOjaccqNkgE7IVbWe1ZFgFOFBjkjwc9aAEPVQW0xG59bErGxwue');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -139,4 +166,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-01-07 15:15:54
+-- Dump completed on 2015-01-08 17:13:48
